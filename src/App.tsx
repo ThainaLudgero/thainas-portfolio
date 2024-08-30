42a0bf08f008eaea7c8ed0d0387249fdf0c2f43c
ThainaLudgero <thainamoreira2005@gmail.com>
2024-08-30 14:52:10 -0300

    feat: Criando componente title

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from './components/title'
+
+function App() {
+  return (
+    <>
+      <Title title="sobre mim" bgcolor="#f9d625" />
+      <Title title="projetos" bgcolor="#6C25F9" />
+      <Title title="fim" bgcolor="#2549F9" />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-blue": "#2549F9",
+        "secondary-yellow": "#f9d625",
+        "terciary-purple": "#6C25F9",
+        "accent-pink": "#F525F9",
+        "white": "#F8F8F8",
+        "black": "#000000"
+      }
+    },
+  },
+  plugins: [],
+}
+
